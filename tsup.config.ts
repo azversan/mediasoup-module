c44e50fc930db3a652112a6d61c11231e55f25cd
Agung Dirgantara <agungmasda29@gmail.com>
2026-08-02 21:21:06 +0700

    build: improve tsup and eslint configuration

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,8 +7,14 @@ export default defineConfig({
     resolve: true,
   },
   splitting: false,
-  sourcemap: true,
+  sourcemap: false,
+  minify: true,
+  treeshake: true,
   clean: true,
   target: 'es2020',
   outDir: 'dist',
+  external: ['@nestjs/common', '@nestjs/core', 'reflect-metadata', 'rxjs', 'mediasoup'],
+  esbuildOptions(options) {
+    options.legalComments = 'none';
+  },
 });
